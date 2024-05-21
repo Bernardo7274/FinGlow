49c7b0a5243ac9ea8936944cf52001f5943e8829
Bernardo7274 <133809860+Bernardo7274@users.noreply.github.com>
2024-05-21 08:13:12 -0500

    Seminars, events and financial panel
    Seminars, events and financial panel

---
+++ lib/domain/models/financial_dashboard_model.dart
@@ -0,0 +1,11 @@
+class DashboardModel{
+  String reportscategory;
+  double incomeamount;
+  double amountexpenses;
+
+  DashboardModel({
+  required this.reportscategory,
+  required this.incomeamount,
+  required this.amountexpenses,
+});
+}
